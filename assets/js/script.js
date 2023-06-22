0945d012b5027e0b6d157bb4979500e0d1501c36
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-06-21 23:29:42 -0400

    added some real questions. added result display after user clicks an answer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,88 @@
 // The question text will be placed in a heading on the page.
-
 var questionHeading = document.querySelector("#question-heading");
 
 // The answers will be buttons inside the div with id "answers".
-
 var answersDiv = document.querySelector("#answers");
 
-// The quiz involves showing a series of questions.
-
-// 1) start the timer
+// When user clicks answer button, show their result for the question
+var resultDiv = document.querySelector("#result");
 
 var countdownText = document.querySelector("#countdown");
 
+
+// The quiz involves showing a series of questions.
+
+function showResultAndContinue(text) {
+  resultDiv.textContent = text;
+
+  setTimeout(() => {
+    var noMoreQuestions = ! showNextQuestion();
+    if (noMoreQuestions) {
+      endQuiz();
+    }
+  }, 2000);
+}
+
+function makeAnswer(text, isCorrect) {
+  var newAnswer = document.createElement("button");
+  answersDiv.appendChild(newAnswer); 
+  newAnswer.textContent = text;
+
+  // add "click" event listener to each answer
+  //    - listener for correct answer
+  //    - different listener for incorrect answers
+
+  if (isCorrect) {
+    newAnswer.addEventListener("click", () => {
+      showResultAndContinue("Correct!");
+    });
+  } else {
+    newAnswer.addEventListener("click", () => {
+      showResultAndContinue("Wrong!");
+    });
+  }
+}
+
+var questionNumber = 0;
+
+function question1() {
+  questionHeading.textContent = "To check if variable `x` is an even number, which expression would you use:";
+  makeAnswer("1. x === 2", false);
+  makeAnswer("2. x / 2", false);
+  makeAnswer("3. (x % 2) === 1", false);
+  makeAnswer("4. (x % 2) === 0", true);
+}
+
+function question2() {
+  questionHeading.textContent = "To join two strings together into a longer string, which would you use:";
+  makeAnswer("1. 'peanut ' . 'butter'", false);
+  makeAnswer("2. 'peanut ' + 'butter'", true);
+  makeAnswer("3. 'peanut ' & 'butter'", false);
+  makeAnswer("4. 'peanut '.join('butter')", false);
+}
+
+function showNextQuestion() {
+  questionNumber++;
+
+  resultDiv.textContent = "";
+  answersDiv.innerHTML = "";
+
+  if (questionNumber === 1) {
+    question1();
+    return true;
+  } else if (questionNumber === 2) {
+    question2();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 1) start the timer
+
+
 var myTimer;
-var countdownTimer = 5;
+var countdownTimer = 25;
 countdownText.textContent = countdownTimer;
 
 function everySecond() {
@@ -24,12 +93,8 @@ function everySecond() {
 
   if (countdownTimer == 0) {
     // When the user runs out of time, go to the Done page
-    allDone();
-    clearInterval(myTimer);
-  } else {
-    // do nothing 
-  }
-
+    endQuiz();
+  } 
 }
 
 myTimer = setInterval(everySecond, 1000);
@@ -37,27 +102,16 @@ myTimer = setInterval(everySecond, 1000);
 // This function will render the points awarded
 // and show the form for entering initials for high score.
 
-function allDone() {
-  questionHeading.textContent = "You're out of time!";
+function endQuiz() {
+  clearInterval(myTimer);
+
+  if (countdownTimer == 0) {
+    questionHeading.textContent = "You're out of time!";
+  } else {
+    questionHeading.textContent = "Congratulations! You answered all questions";
+  }
 
   answersDiv.innerHTML = `<p>Your score: ${countdownTimer}</p>`;
 }
 
-
-
-// 2) show next question and answers
-
-// 3) add "click" event listener to each answer
-//    - listener for correct answer
-//    - different listener for incorrect answers
-
-
-
-questionHeading.textContent = "This is the question!";
-
-var newAnswer = document.createElement("button");
-answersDiv.appendChild(newAnswer); 
-newAnswer.textContent = "New answer";
-
-
-
+showNextQuestion();
